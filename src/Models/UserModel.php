46d7910ca4daffe3c01bbd84b7986fa079104e57
hvd0706 <hvd07062002@gmail.com>
2023-11-01 16:45:25 +0700

    Thêm chức năng cập nhật avatar và chỉnh sửa thông tin

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -43,11 +43,28 @@ class UserModel
     public function updatePass($email, $password)
     {
         include SRC_DIR . '/config.php';
-
         $sql = "UPDATE khach_hang SET mat_khau = ? WHERE email = ?";
         $stmt = $conn->prepare($sql);
         $password = md5($password);
         $stmt->execute([$password, $email]);
+        return $stmt->rowCount() === 1;
+    }
+
+    public function updateAvatar($id, $avatar)
+    {
+        include SRC_DIR . '/config.php';
+        $sql = "UPDATE khach_hang SET avatar = ? WHERE id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([BASE_URL . '/uploads/' . $avatar, $id]);
+        return $stmt->rowCount() === 1;
+    }
+
+    public function updateProfile($id, $name, $phone, $address)
+    {
+        include SRC_DIR . '/config.php';
+        $sql = "UPDATE khach_hang SET ho_ten=?, so_dien_thoai=?, dia_chi=? WHERE id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$name, $phone, $address, $id]);
         return $stmt->rowCount() === 1;
     }
 }
