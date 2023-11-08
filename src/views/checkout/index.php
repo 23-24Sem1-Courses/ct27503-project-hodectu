05c859b8d9d0b4e769f04d0e1410b2780d4df29e
hvd0706 <hvd07062002@gmail.com>
2023-11-08 09:20:25 +0700

    Cập nhật giao diện

--- src/views/checkout/index.php
+++ src/views/checkout/index.php
@@ -15,36 +15,36 @@
                     <span class="text-muted">Giỏ hàng</span>
                     <span class="badge badge-secondary badge-pill">2</span>
                 </h4>
-                <ul class="checkout-cart list-group mb-3 overflow-y-scroll" style="height: 350px;">
-                    <?php $totalPrice = 0; ?>
-                    <?php foreach ($cartList as $item) :
-                        $totalPrice += $item['so_luong'] * $item['gia_sale'];
-                    ?>
-                        <li class="list-group-item d-flex justify-content-between lh-condensed">
-                            <div class="d-flex justify-content-between">
-                                <a href class="pan d-flex align-items-center" data-big="<?= htmlspecialchars($item['anh_bia']) ?>">
-                                    <img src="<?= htmlspecialchars($item['anh_bia']) ?>" alt="" data-action="zoom" class="me-2 img-fluid" style="width: 50px;">
-                                </a>
-                                <div>
-                                    <p class="mb-0"><?= htmlspecialchars($item['ten_sach']) ?></p>
-                                    <p class="mb-0 text-muted">
-                                        <span><?= htmlspecialchars(format_money($item['gia_sale'])) ?></span>
-                                        <i class="fa-solid fa-xmark"></i>
-                                        <span><?= htmlspecialchars($item['so_luong']) ?></span>
-                                    </p>
-                                </div>
-                            </div>
-                            <span class="text-muted">
-                                <?= htmlspecialchars(format_money($item['so_luong'] * $item['gia_sale'])) ?>
-                            </span>
-                        </li>
-                    <?php endforeach ?>
-                </ul>
                 <?php if (!empty($cartList)) { ?>
-                    <div class="d-flex justify-content-between px-2">
-                        <span>Tổng tiền</span>
-                        <strong><?= htmlspecialchars(format_money($totalPrice)) ?></strong>
-                    </div>
+                    <ul class="checkout-cart list-group mb-3 overflow-y-scroll" style="height: 350px;">
+                        <?php $totalPrice = 0; ?>
+                        <?php foreach ($cartList as $item) :
+                            $totalPrice += $item['so_luong'] * $item['gia_sale'];
+                        ?>
+                            <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <div class="d-flex justify-content-between">
+                                    <a href class="pan d-flex align-items-center" data-big="<?= htmlspecialchars($item['anh_bia']) ?>">
+                                        <img src="<?= htmlspecialchars($item['anh_bia']) ?>" alt="" data-action="zoom" class="me-2 img-fluid" style="width: 50px;">
+                                    </a>
+                                    <div>
+                                        <p class="mb-0"><?= htmlspecialchars($item['ten_sach']) ?></p>
+                                        <p class="mb-0 text-muted">
+                                            <span><?= htmlspecialchars(format_money($item['gia_sale'])) ?></span>
+                                            <i class="fa-solid fa-xmark"></i>
+                                            <span><?= htmlspecialchars($item['so_luong']) ?></span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <span class="text-muted">
+                                    <?= htmlspecialchars(format_money($item['so_luong'] * $item['gia_sale'])) ?>
+                                </span>
+                            </li>
+                        <?php endforeach ?>
+                        <li class="total-price list-group-item d-flex justify-content-between px-2 position-sticky bottom-0">
+                            <span>Tổng tiền</span>
+                            <strong><?= htmlspecialchars(format_money($totalPrice)) ?></strong>
+                        </li>
+                    </ul>
                 <?php } else { ?>
                     <div class="d-flex justify-content-center align-items-center h-100">
                         <img src="/images/cart/empty-cart.png" alt="" class="img-fluid" style="width: 250px;">
@@ -123,6 +123,9 @@
                                                     <img src="/images/cart/empty-cart.png" alt="" class="img-fluid" style="width: 250px;">
                                             </div>`;
                         $('.checkout-cart').html(emptyCart);
+                        if (!res['error']) {
+                            window.location.href = '/purchase'
+                        }
                     })
                 })
         }
