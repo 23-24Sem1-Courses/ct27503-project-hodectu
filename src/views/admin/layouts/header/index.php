a0f92950cd38d838ff3c292ef793a177cc7758aa
hvd0706 <hvd07062002@gmail.com>
2023-11-08 21:49:15 +0700

    Cập nhật giao diện

--- src/views/admin/layouts/header/index.php
+++ src/views/admin/layouts/header/index.php
@@ -7,11 +7,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="/images/favicon/favicon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/js/jquery.pan-master/dist/css/jquery.pan.css">
+
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.20.0/jquery.validate.min.js" integrity="sha512-WMEKGZ7L5LWgaPeJtw9MBM4i5w5OSBlSjTjCtSnvFJGSVD26gE5+Td12qN5pvWXhuWaWcVwF++F7aqu9cvqP0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="/js/jquery.pan-master/dist/jquery.pan.min.js"></script>
     <link rel="stylesheet" href="/css/style.css">
 
     <style>
@@ -46,6 +49,7 @@
                         <li class="nav-item d-flex gap-2 align-items-center">
                             <a class=" text-white nav-link active fs-5" href="/admin"><i class="fa-solid fa-house"></i></a>
                             <a class=" text-white nav-link active fs-5" href="/admin/add"><i class="fa-solid fa-plus"></i> sản phẩm</a>
+                            <a class="text-white nav-link active fs-5" href="/admin/order">Đơn hàng<i class="fa-solid fa-right-from-bracket ms-1"></i></a>
                         </li>
                     </ul>
 
